41aa15480f0a1a0dd8bab3579218d827300c0d0c
juliandiaz23 <jadiaztrujillo@uts.edu.co>
2026-03-04 23:09:50 -0500

    fix port render

--- server.js
+++ server.js
@@ -276,8 +276,8 @@ app.get("/reporte", (req, res) => {
 
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Servidor iniciado en http://localhost:3000");
-
+app.listen(PORT, () => {
+    console.log("Servidor iniciado en puerto " + PORT);
 });
